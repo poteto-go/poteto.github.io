af6287863d4f49841a6d7be9d881f855691bc638
poteto0 <mahiro.fukuda.aries@gmail.com>
2025-01-15 19:15:02 +0900

    BUG: fix browser router

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = () => {
       <Header />
       <div className="main-container">
         <Sidebar />
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename={"/poteto.github.io"}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/router" element={<AboutRouter />} />
